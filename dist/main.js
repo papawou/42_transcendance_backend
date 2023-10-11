d455b00b8471c6526a5e3f0ca3dfe448143bd622
Jeremie Lale-demoz <jlale-de@c3r2s5.42lausanne.ch>
2023-10-11 19:33:35 +0200

    prisma ..

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const prismaClient_1 = require("./prismaClient");
 async function bootstrap() {
+    const data = await prismaClient_1.default.post.findMany();
+    console.log(data);
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(3000);
 }
+;
 bootstrap();
 //# sourceMappingURL=main.js.map
