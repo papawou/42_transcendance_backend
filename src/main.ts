217c68a42f2a446695a15064b6a43bb1497b6223
jb <jb.aubareau@gmail.com>
2023-10-19 18:25:12 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const corsOptions = {
+    origin:'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
+
 bootstrap();
