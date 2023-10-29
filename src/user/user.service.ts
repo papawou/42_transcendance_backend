41d1cb1d35446890325675b7a9151424d31980d1
jb <jb.aubareau@gmail.com>
2023-10-29 15:48:32 +0100

    add delete

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 @Injectable()
 export class UserService {
+    
+    addFriend = async (id1: number, id2: number) => {
+        if (id1 === id2) {
+            throw new Error("");
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: { id: id1 },
+            include: { friends: true },
+            data: {
+                friends: {
+                connect: { id: id2 },
+                },
+            },
+        });
+        const updatedUser2 = await prisma.user.update({
+            where: { id: id2 },
+            include: { friends: true },
+            data: {
+                friends: {
+                connect: { id: id1 },
+                },
+            },
+        });
+    }
+
+    deleteFriend = async (id1: number, id2: number) => {
+        const updatedUser = await prisma.user.update({
+            where: { id: id1 },
+            include: { friends: true },
+            data: {
+                friends: {
+                disconnect: { id: id2 },
+                },
+            },
+        });
+        const updatedUser2 = await prisma.user.update({
+            where: { id: id2 },
+            include: { friends: true },
+            data: {
+                friends: {
+                disconnect: { id: id1 },
+                },
+            },
+        });
+    }
 }
