d7c3a8e59a82a8e7ee791a068e32fc0627d0701a
Stannt <97431057+Stannt@users.noreply.github.com>
2023-11-07 15:08:56 +0100

    add delete (#4)
    Co-authored-by: jb <jb.aubareau@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-@Controller('user')
+
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
     async getUsers(){
-    const users = await prisma.user.findMany();
+    const users = (await prisma.user.findMany());
     return users;
   }
+
+  @Get(':id/friends')
+    async getFriends(@Param('id') userId: string) {
+    const uid = Number(userId)
+    const user = await prisma.user.findUnique({
+      where: { id: uid },
+      include: { friends: true }
+    });
+
+    if (!user) {
+      throw new Error(`Utilisateur avec l'ID ${userId} non trouvé.`);
+    }
+    return user.friends;
+  }
+
+  @Post(':id/add-friend/:friendId')
+    async addFriend(
+    @Param('id') userId: string,
+    @Param('friendId') friendId: string,
+  ) {
+    const id1 = Number(userId);
+    const id2 = Number(friendId);
+
+    const updatedUser = await this.userService.addFriend(id1, id2);
+
+    return updatedUser;
+  }
+
+  @Post(':id/delete-friend/:friendId')
+    async deleteFriend(
+      @Param('id') userId: string,
+      @Param('friendId') friendId: string,
+    ) {
+      const id1 = Number(userId);
+      const id2 = Number(friendId)
+
+      const updatedUser = await this.userService.deleteFriend(id1, id2);
+
+      return updatedUser;
+    }
 }
