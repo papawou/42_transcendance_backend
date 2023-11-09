50e9e011a7fd3855312cf53f5d8e2c471f62abb6
Stannt <97431057+Stannt@users.noreply.github.com>
2023-11-09 16:43:25 +0100

    add delete (#5)
    Co-authored-by: jb <jb.aubareau@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,51 +6,134 @@ const prisma = new PrismaClient()
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) { }
 
-  @Get()
-    async getUsers(){
-    const users = (await prisma.user.findMany());
-    return users;
-  }
+	@Get()
+	async getUsers() {
+		const users = (await prisma.user.findMany());
+		return users;
+	}
 
-  @Get(':id/friends')
-    async getFriends(@Param('id') userId: string) {
-    const uid = Number(userId)
-    const user = await prisma.user.findUnique({
-      where: { id: uid },
-      include: { friends: true }
-    });
+	@Get(':id/user')
+	async getUser(@Param('id') id: string) {
+		const uid = Number(id);
 
-    if (!user) {
-      throw new Error(`Utilisateur avec l'ID ${userId} non trouvé.`);
-    }
-    return user.friends;
-  }
+		const user = prisma.user.findUnique({
+			where: {id: uid},
+		})
 
-  @Post(':id/add-friend/:friendId')
-    async addFriend(
-    @Param('id') userId: string,
-    @Param('friendId') friendId: string,
-  ) {
-    const id1 = Number(userId);
-    const id2 = Number(friendId);
+		return user;
+	}
 
-    const updatedUser = await this.userService.addFriend(id1, id2);
+/*------------------------------FRIENDS--------------------------*/
 
-    return updatedUser;
-  }
+	@Get(':id/friends')
+	async getFriends(@Param('id') userId: string) {
+		const uid = Number(userId)
+		const user = await prisma.user.findUnique({
+			where: { id: uid },
+			include: { friends: true }
+		});
 
-  @Post(':id/delete-friend/:friendId')
-    async deleteFriend(
-      @Param('id') userId: string,
-      @Param('friendId') friendId: string,
-    ) {
-      const id1 = Number(userId);
-      const id2 = Number(friendId)
+		if (!user) {
+			throw new Error(`Utilisateur avec l'ID ${userId} non trouvé.`);
+		}
+		return user.friends;
+	}
 
-      const updatedUser = await this.userService.deleteFriend(id1, id2);
+	@Post(':id/add-friend/:friendId')
+	async addFriend(
+		@Param('id') userId: string,
+		@Param('friendId') friendId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(friendId);
 
-      return updatedUser;
-    }
+		const updatedUser = await this.userService.addFriend(id1, id2);
+
+		return updatedUser;
+	}
+
+	@Post(':id/delete-friend/:friendId')
+	async deleteFriend(
+		@Param('id') userId: string,
+		@Param('friendId') friendId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(friendId)
+
+		const updatedUser = await this.userService.deleteFriend(id1, id2);
+
+		return updatedUser;
+	}
+
+/*------------------------------BLOCK--------------------------*/
+
+	@Post(':id/block-user/:blockedUserId')
+	async blockUser(
+		@Param('id') userId: string,
+		@Param('blockedUserId') blockedUserId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(blockedUserId);
+
+		const updatedUser = await this.userService.blockUser(id1, id2);
+
+		return updatedUser;
+	}
+
+	@Post(':id/unblock-user/:blockedUserId')
+	async unblockUser(
+		@Param('id') userId: string,
+		@Param('blockedUserId') blockedUserId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(blockedUserId);
+
+		const updatedUser = await this.userService.unblockUser(id1, id2);
+
+		return updatedUser;
+	}
+
+	@Get(':id/blocked')
+	async getBlocked(@Param('id') userId: string) {
+		const uid = Number(userId)
+		const user = await prisma.user.findUnique({
+			where: { id: uid },
+			include: { blocked: true }
+		});
+
+		if (!user) {
+			throw new Error(`Utilisateur avec l'ID ${userId} non trouvé.`);
+		}
+		return user.blocked;
+	}
+
+/*------------------------------FRIEND-REQUEST--------------------------*/
+
+	@Post(':id/send-friend-request/:friendId')
+	async sendFriendRequest(
+		@Param('id') userId: string,
+		@Param('friendId') friendId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(friendId);
+
+		const updatedUser = await this.userService.sendFriendRequest(id1, id2);
+
+		return updatedUser;
+	}
+
+	@Post(':id/refuse-friend-request/:friendId')
+	async refuseFriendRequest(
+		@Param('id') userId: string,
+		@Param('friendId') friendId: string,
+	) {
+		const id1 = Number(userId);
+		const id2 = Number(friendId);
+
+		const updatedUser = await this.userService.refuseFriendRequest(id1, id2);
+
+		return updatedUser;
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,54 +1,163 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { error } from 'console';
 
 const prisma = new PrismaClient()
 
 @Injectable()
 export class UserService {
-    
-    addFriend = async (id1: number, id2: number) => {
-        if (id1 === id2) {
-            throw new Error("");
-        }
 
-        const updatedUser = await prisma.user.update({
-            where: { id: id1 },
-            include: { friends: true },
-            data: {
-                friends: {
-                connect: { id: id2 },
-                },
-            },
-        });
-        const updatedUser2 = await prisma.user.update({
-            where: { id: id2 },
-            include: { friends: true },
-            data: {
-                friends: {
-                connect: { id: id1 },
-                },
-            },
-        });
-    }
+	sendFriendRequest = async (id1: number, id2: number) => {
+		if (id1 === id2) {
+			throw new Error(`Utilisateur ${id1} ne peut pas ajouter l'utilisateur ${id2}`);
+		}
 
-    deleteFriend = async (id1: number, id2: number) => {
-        const updatedUser = await prisma.user.update({
-            where: { id: id1 },
-            include: { friends: true },
-            data: {
-                friends: {
-                disconnect: { id: id2 },
-                },
-            },
-        });
-        const updatedUser2 = await prisma.user.update({
-            where: { id: id2 },
-            include: { friends: true },
-            data: {
-                friends: {
-                disconnect: { id: id1 },
-                },
-            },
-        });
-    }
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					pending: {
+						connect: { id: id2 },
+					},
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					pendingOf: {
+						connect: { id: id1 },
+					},
+				},
+			}),
+		]);
+	}
+
+	refuseFriendRequest = async (id1: number, id2: number) => {
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					pending: {
+						disconnect: { id: id2 },
+					},
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					pendingOf: {
+						disconnect: { id: id1 },
+					},
+				},
+			}),
+		]);
+	}
+
+
+	addFriend = async (id1: number, id2: number) => {
+
+		this.refuseFriendRequest(id1, id2);
+
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					friends: { connect: { id: id2 } }
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					friends: { connect: { id: id1 } }
+				},
+			}),
+		]);
+	}
+
+	deleteFriend = async (id1: number, id2: number) => {
+
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					friends: {
+						disconnect: { id: id2 }
+					},
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					friends: {
+						disconnect: { id: id1 }
+					},
+				},
+			}),
+		]);
+	}
+
+	blockUser = async (id1: number, id2: number) => {
+
+		const areFriends = prisma.user.findUnique({
+			where: { id: id1 },
+			select: { friends: { where: { id: id2 } } }
+		});
+
+		if (areFriends) {
+			this.deleteFriend(id1, id2);
+		}
+
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					blocked: {
+						connect: { id: id2 }
+					},
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					blockedOf: {
+						connect: { id: id1 }
+					},
+				},
+			}),
+		]);
+	}
+
+	unblockUser = async (id1: number, id2: number) => {
+
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { id: id1 },
+				data: {
+					blocked: {
+						disconnect: { id: id2 }
+					},
+				},
+			}),
+			prisma.user.update({
+				where: { id: id2 },
+				data: {
+					blockedOf: {
+						disconnect: { id: id1 }
+					},
+				},
+			}),
+		]);
+	}
+
+	isBlocked = async (id1: number, id2: number) => {
+
+		const isblocked = prisma.user.findUnique({
+			where: { id: id1 },
+			select: { blocked: { where: { id: id2 } } }
+		});
+
+		if (isblocked)
+			return true;
+		return false;
+	}
 }
