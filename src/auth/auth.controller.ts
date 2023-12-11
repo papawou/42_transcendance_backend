028a411c897c48cd0d015d66ffd11951c8753899
MateyN <mateynnikolov@gmail.com>
2023-12-11 13:09:12 +0100

    2fa another try

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post, Get, Req, UnauthorizedException, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, Get, Req, Put, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { isDef } from "src/technical/isDef";
 import { AuthService } from "./auth.service";
-import { LoginDTO } from "./auth.dto";
+import { LoginDTO, EnableTwoFactorDTO, ValidateTwoFactorDTO, DisableTwoFactorDTO} from "./auth.dto";
 import { from, lastValueFrom, map } from "rxjs";
 import { Request } from 'express';
 import { HttpService } from "@nestjs/axios";
 import { URLSearchParams } from "url";
 import { ConfigModule } from "@nestjs/config";
 import prisma from "@/database/prismaClient";
+import speakeasy from 'speakeasy';
+import qrcode from 'qrcode';
 
 @Controller('auth')
 export class AuthController {
@@ -23,11 +25,22 @@ export class AuthController {
         }
         return this.authService.login(user);
     }
+    
+    @Put('2fa/enable')
+    async enableTwoFactor(@Body() enableTwoFactorDTO: EnableTwoFactorDTO) {
+        return await this.authService.enableTwoFactor(enableTwoFactorDTO);
+    }
 
-    // GET auth/ft/callback
-    @Get('ft/callback')
-    async handleFtCallback() {
-      return { msg: 'ft callback' };
+    // POST auth/2fa/validate
+    @Post('2fa/validate')
+    async validateTwoFactor(@Body() validateTwoFactorDTO: ValidateTwoFactorDTO) {
+        return await this.authService.validateTwoFactor(validateTwoFactorDTO);
+    }
+
+    // POST auth/2fa/disable
+    @Post('2fa/disable')
+    async disableTwoFactor(@Body() disableTwoFactorDTO: DisableTwoFactorDTO) {
+        return await this.authService.disableTwoFactor(disableTwoFactorDTO);
     }
 
     // POST auth/ft/callback
@@ -61,19 +74,25 @@ export class AuthController {
         // Save user in db
         // || Check user in db + if tfa is enabled
         const findUser = await prisma.user.findUnique({ where: { name: get.login } });
-        if ( findUser ) {
+        if ( findUser && !findUser.twoFactorEnabled) {
+            const secretKey = speakeasy.generateSecret({ length: 20 }).base32;
+
+            const otpAuthUrl = speakeasy.otpauthURL({
+                secret: secretKey,
+                label: 'Transcendence',
+                issuer: 'Transcendence',
+            });
+            const qrCodeImage = await qrcode.toDataURL(otpAuthUrl);
             console.log(">>> user already in db");
             await prisma.user.update({ // prevent duplicate
                 where: { name: get.login },
                 data: { 
                     ft_id: '' + get.id,
                     pic: get.image.link,
+                    twoFactorEnabled: true,
+                    secretKey: secretKey,
             }, });
-            // MATT : TFA HERE
-            // if (hasEnabledTFA(get.login)) {
-            // then send email with tfa code
-            // then return tfa code
-            // }
+            return { qrCodeImage };
         } else {
             console.log(">>> user not in db");
             await prisma.user.create({ // save user info in db
@@ -81,6 +100,8 @@ export class AuthController {
                     name: get.login,
                     ft_id: '' + get.id,
                     pic: get.image.link,
+                    twoFactorEnabled: false,
+                    secretKey: null,
              }, });
         }
         // kenneth : TODO what about access_token and refresh_token and cookie?
