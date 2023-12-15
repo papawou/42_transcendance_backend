b92be048e533fbc407951ed79e94d56c16d1a131
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2023-12-15 13:02:19 +0100

    rebase oauth2 2fa2

---
+++ prisma/migrations/20231121084136_init/migration.sql
@@ -0,0 +1,86 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "ft_id" TEXT NOT NULL DEFAULT 'couz',
+    "pic" TEXT NOT NULL DEFAULT 'https://cdn.intra.42.fr/doorkeeper/application/image/16985/Screen_Shot_2023-10-25_at_9.35.44_AM.png',
+    "wins" INTEGER NOT NULL DEFAULT 0,
+    "loses" INTEGER NOT NULL DEFAULT 0,
+    "rank" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT,
+    "player1Score" INTEGER NOT NULL,
+    "player2Score" INTEGER NOT NULL,
+    "player1Id" INTEGER NOT NULL,
+    "player2Id" INTEGER NOT NULL,
+    "player1Name" TEXT NOT NULL,
+    "player2Name" TEXT NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_friends" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_blocked" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_pending" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_friends_AB_unique" ON "_friends"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_friends_B_index" ON "_friends"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_blocked_AB_unique" ON "_blocked"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_blocked_B_index" ON "_blocked"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_pending_AB_unique" ON "_pending"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_pending_B_index" ON "_pending"("B");
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_player1Id_fkey" FOREIGN KEY ("player1Id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_blocked" ADD CONSTRAINT "_blocked_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_blocked" ADD CONSTRAINT "_blocked_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_pending" ADD CONSTRAINT "_pending_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_pending" ADD CONSTRAINT "_pending_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Post, Get, Req, Put, UnauthorizedException, UseGuards } from "@nestjs/common";
+import { Body, Controller, Put, Post, Get, Req, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { isDef } from "src/technical/isDef";
 import { AuthService } from "./auth.service";
-import { LoginDTO, EnableTwoFactorDTO, ValidateTwoFactorDTO, DisableTwoFactorDTO} from "./auth.dto";
-import { from, lastValueFrom, map } from "rxjs";
+import { LoginDTO, EnableTwoFactorDTO, ValidateTwoFactorDTO, DisableTwoFactorDTO} from "./auth.dto";import { from, lastValueFrom, map } from "rxjs";
 import { Request } from 'express';
 import { HttpService } from "@nestjs/axios";
 import { URLSearchParams } from "url";
 import { ConfigModule } from "@nestjs/config";
 import prisma from "@/database/prismaClient";
+import axios from "axios";
 import speakeasy from 'speakeasy';
 import qrcode from 'qrcode';
 
 @Controller('auth')
 export class AuthController {
     constructor(
-        private authService: AuthService, 
+        private authService: AuthService,
         private httpService: HttpService) { }
 
     @Post('login')
@@ -25,7 +25,7 @@ export class AuthController {
         }
         return this.authService.login(user);
     }
-    
+
     @Put('2fa/enable')
     async enableTwoFactor(@Body() enableTwoFactorDTO: EnableTwoFactorDTO) {
         return await this.authService.enableTwoFactor(enableTwoFactorDTO);
@@ -42,10 +42,21 @@ export class AuthController {
     async disableTwoFactor(@Body() disableTwoFactorDTO: DisableTwoFactorDTO) {
         return await this.authService.disableTwoFactor(disableTwoFactorDTO);
     }
+    
+    // GET auth/ft/callback
+    @Get('ft/callback')
+    async handleFtCallback() {
+      return { msg: 'ft callback' };
+    }
 
     // POST auth/ft/callback
     @Post('ft/callback')
     async ftCallback(@Req() req: Request) {
+        console.log("@post auth/ft/callback");
+        if(!isDef(req.body.code)) {
+            console.log(">>> no code in body");
+            throw new UnauthorizedException(">>> no code in body");
+        }
         const params = new URLSearchParams([
             ['grant_type', 'authorization_code'],
             ['client_id', process.env.FT_CLIENT_ID],
@@ -56,55 +67,89 @@ export class AuthController {
 
         // Exchange your code for an access token
         const endpoint_post = "https://api.intra.42.fr/oauth/token?" + params.toString();
-        const post = await lastValueFrom(this.httpService.post(endpoint_post).pipe(
-            map((resp) => {
+        const post = await axios
+            .post(endpoint_post)
+            .then((resp) => {
                 return resp.data;
-            }),
-        ));
+            })
+            .catch((err) => {
+                console.log({
+                    where: "auth.controller.ts@post.catch",
+                    status: err.response.status,
+                    statusText: err.response.statusText,
+                    data: err.response.data,
+                });
+            });
+        if (!isDef(post)) {
+            console.log({
+                where: "auth.controller.ts > if (!isDef(post))",
+                status: "STRANGE BEHAVIOR",
+                what: ">>> no access_token in post",
+                warning: "This error message can be triggered by the first useEffect hook run which has no repercussion whatsoever for the second run, see AuthFtCallback.tsx l.15"
+            });
+            return ;
+        }
 
         // Make API requests with your token
-        const get = await lastValueFrom(this.httpService.get('https://api.intra.42.fr/v2/me',{
-            headers: { Authorization: `Bearer ${post.access_token}`,},
-        }).pipe(
-            map((resp) => {
-                if (post.access_token) return resp.data;
-            }),
-        ));
-
+        const get = await axios
+            .get('https://api.intra.42.fr/v2/me', {
+                headers: { 'Authorization': `Bearer ${post.access_token}` }
+            })
+            .then((resp) => {
+                if (isDef(post.access_token)) return resp.data;
+            })
+            .catch((err) => {
+                console.log({
+                    where: "auth.controller.ts@get.catch",
+                    status: err.response.status,
+                    statusText: err.response.statusText,
+                    data: err.response.data,
+                });
+            });
+        
         // Save user in db
         // || Check user in db + if tfa is enabled
         const findUser = await prisma.user.findUnique({ where: { name: get.login } });
-        if ( findUser && !findUser.twoFactorEnabled) {
-            const secretKey = speakeasy.generateSecret({ length: 20 }).base32;
-
-            const otpAuthUrl = speakeasy.otpauthURL({
-                secret: secretKey,
-                label: 'Transcendence',
-                issuer: 'Transcendence',
-            });
-            const qrCodeImage = await qrcode.toDataURL(otpAuthUrl);
+        if ( isDef(findUser) ) {
             console.log(">>> user already in db");
-            await prisma.user.update({ // prevent duplicate
-                where: { name: get.login },
-                data: { 
-                    ft_id: '' + get.id,
-                    pic: get.image.link,
-                    twoFactorEnabled: true,
-                    secretKey: secretKey,
-            }, });
-            return { qrCodeImage };
-        } else {
-            console.log(">>> user not in db");
-            await prisma.user.create({ // save user info in db
-                data: { 
-                    name: get.login,
-                    ft_id: '' + get.id,
-                    pic: get.image.link,
-                    twoFactorEnabled: false,
-                    secretKey: null,
-             }, });
+            // MATT : TFA HERE
+            // if (hasEnabledTFA(get.login)) {
+            // then send email with tfa code
+            // then return tfa code
+            // }
+        // if ( findUser && !findUser.twoFactorEnabled) {
+        //     const secretKey = speakeasy.generateSecret({ length: 20 }).base32;
+
+        //     const otpAuthUrl = speakeasy.otpauthURL({
+        //         secret: secretKey,
+        //         label: 'Transcendence',
+        //         issuer: 'Transcendence',
+        //     });
+        //     const qrCodeImage = await qrcode.toDataURL(otpAuthUrl);
+        //     console.log(">>> user already in db");
+        //     await prisma.user.update({ // prevent duplicate
+        //         where: { name: get.login },
+        //         data: { 
+        //             ft_id: '' + get.id,
+        //             pic: get.image.link,
+        //             twoFactorEnabled: true,
+        //             secretKey: secretKey,
+        //     }, });
+        //     return { qrCodeImage };
+            return this.authService.login(findUser); // return jwt
         }
-        // kenneth : TODO what about access_token and refresh_token and cookie?
-        return get;
+        console.log(">>> user not in db");
+        const createUser = await prisma.user.create({ // save user info in db
+            data: {
+                name: get.login, // ex. krioja
+                ft_id: '' + get.id, // ex. 123456
+                pic: get.image.link, // ex. https://cdn.intra.42.fr/users/krioja.jpg
+                // twoFactorEnabled: false,
+                // secretKey: null,
+            },
+        });
+        if (!isDef(createUser))
+            throw new UnauthorizedException(">>> error while creating user in db");
+        return this.authService.login(createUser); // return jwt
     }
 }
